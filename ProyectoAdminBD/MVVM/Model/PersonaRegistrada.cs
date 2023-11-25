b9fd1821fe83900b9fe916cbe2f886f0d29d0320
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2023-11-25 05:35:39 -0600

    Started implementation of Persona_registrada

---
+++ ProyectoAdminBD/MVVM/Model/PersonaRegistrada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAdminBD.MVVM.Model
+{
+    class PersonaRegistrada
+    {
+        public string Curp { get; set; }
+        public string Municipio { get; set; }
+        public string Genero { get; set; }
+        public string Presentado { get; set; }
+        public int No_oficialia { get; set; }
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public DateTime FechaNac {  get; set; }
+        public DateTime HoraNac { get; set; }
+        public string Crip {  get; set; }
+        public int No_acta {  get; set; }
+        public int No_libro { get; set; }
+        public DateTime FechaReg {  get; set; }
+    }
+}
