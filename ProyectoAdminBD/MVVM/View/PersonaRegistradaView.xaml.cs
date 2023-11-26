7f1b3401d2acd9b1c10bc4c7f4112762a5d74245
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2023-11-26 02:40:53 -0600

    insert in persona_registrada kinda working

--- ProyectoAdminBD/MVVM/View/PersonaRegistradaView.xaml.cs
+++ ProyectoAdminBD/MVVM/View/PersonaRegistradaView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using ProyectoAdminBD.Connection;
 using ProyectoAdminBD.MVVM.Model;
 using ProyectoAdminBD.Theme;
 using System;
@@ -41,6 +42,8 @@ namespace ProyectoAdminBD.MVVM.View
         public PersonaRegistradaView()
         {
             InitializeComponent();
+            var appConfig = new LoadConfig();
+            _configuration = appConfig.Configuration;
             holder = DataHolder.Instance;
             MUNICIPIO.ItemsSource = GetMunicipios();
             GENERO.ItemsSource = GetGeneros();
@@ -80,7 +83,7 @@ namespace ProyectoAdminBD.MVVM.View
 
         private List<Municipio> GetMunicipios()
         {
-            return new QueryExecutor().ExecuteQuery(
+            List<Municipio> temp =  new QueryExecutor().ExecuteQuery(
                 "SELECT * FROM municipio",
                 row => new Municipio
                 {
@@ -89,6 +92,8 @@ namespace ProyectoAdminBD.MVVM.View
                     _name = row["nombre"].ToString()
                 }
                 );
+            municipios = temp;
+            return municipios;
         }
 
         private List<Genero> GetGeneros()
@@ -148,8 +153,154 @@ namespace ProyectoAdminBD.MVVM.View
 
         private void ExecOperation(object sender, RoutedEventArgs e)
         {
-            
+            string curp = CURP.Text;
+            string municipio = MUNICIPIO?.SelectedValue.ToString();
+            string? genero = GENERO?.SelectedValue?.ToString();
+            string? presentado = PRESENTADO?.SelectedValue?.ToString();
+            string? oficialia = OFICIALIA?.SelectedValue?.ToString();
+            string nombre = NOMBRE.Text;
+            string apPaterno = APPATERNO.Text;
+            string apMaterno = APMATERNO.Text;
+            string curpPadre = CURPPADRE.Text;
+            string curpMadre = CURPMADRE.Text;
+            DateTime? fechaNac = FECHANAC.SelectedDate;
+            TimeSpan? horaNac = TimeSpan.Parse(HORANAC.Text);
+            string crip = CRIP.Text;
+            string numActa = NUMACTA.Text;
+            string numLibro = NUMLIBRO.Text;
+            DateTime? fechaReg = FECHAREG.SelectedDate;
+            Debug.WriteLine($"CURP: {curp}");
+            Debug.WriteLine($"Municipio: {municipio}");
+            Debug.WriteLine($"Genero: {genero}");
+            Debug.WriteLine($"Presentado: {presentado}");
+            Debug.WriteLine($"Oficialia: {oficialia}");
+            Debug.WriteLine($"Nombre: {nombre}");
+            Debug.WriteLine($"Apellido Paterno: {apPaterno}");
+            Debug.WriteLine($"Apellido Materno: {apMaterno}");
+            Debug.WriteLine($"CURP Padre: {curpPadre}");
+            Debug.WriteLine($"CURP Madre: {curpMadre}");
+            Debug.WriteLine($"Fecha de Nacimiento: {fechaNac}");
+            Debug.WriteLine($"Hora de Nacimiento: {horaNac}");
+            Debug.WriteLine($"CRIP: {crip}");
+            Debug.WriteLine($"Número de Acta: {numActa}");
+            Debug.WriteLine($"Número de Libro: {numLibro}");
+            Debug.WriteLine($"Fecha de Registro: {fechaReg}");
+            string context = (sender as Button).Content.ToString().ToUpper();
+            using (SqlConnection conn = new SqlConn(_configuration).GetConnection())
+            {
+                conn.Open();
+                string q;
+                switch (context)
+                {
+                    case "GUARDAR":
+                        if(!string.IsNullOrEmpty(curp))
+                            new ShoInfoMsg(ShoInfoMsg.WARNING, "Se ha detectado que el campo curp contiene texto, este sera sobreescrito al generado automaticamente");
+                        
+                        if (string.IsNullOrEmpty(municipio) ||
+                            string.IsNullOrEmpty(genero) || string.IsNullOrEmpty(presentado) ||
+                            string.IsNullOrEmpty(oficialia) || string.IsNullOrEmpty(nombre) ||
+                            string.IsNullOrEmpty(apPaterno) || string.IsNullOrEmpty(apMaterno) ||
+                            string.IsNullOrEmpty(curpPadre) || string.IsNullOrEmpty(curpMadre) ||
+                            fechaNac == null || horaNac == null ||
+                            string.IsNullOrEmpty(crip) || string.IsNullOrEmpty(numActa) ||
+                            string.IsNullOrEmpty(numLibro) || fechaReg == null)
+                        {
+                            new ShoInfoMsg(ShoInfoMsg.ERROR, "1 o mas campos vacios, por favor intente de nuevo!");
+                            return;
+                        }
+                        string nuCurp = GenerarCurp(apPaterno, apMaterno, nombre, fechaNac, genero, municipio);
+                        Debug.WriteLine(nuCurp);
+                        q = $"SELECT * FROM persona_registrada where curp = '{nuCurp}'";
+                        if (VerifyExistingValue(conn, q))
+                        {
+                            new ShoInfoMsg(ShoInfoMsg.ERROR, "Dato ya existe en la BD! intente de nuevo");
+                            return;
+                        }
+                        if(!VerifyExistingValue(conn, $"SELECT * FROM padre WHERE curp = '{curpPadre}'"))
+                        {
+                            new ShoInfoMsg(ShoInfoMsg.ERROR,"Curp de padre no existente!");
+                            return;
+                        }
+                        else if(!VerifyExistingValue(conn,$"SELECT * FROM madre WHERE curp = '{curpMadre}'"))
+                        {
+                            new ShoInfoMsg(ShoInfoMsg.ERROR, "Curp de madre no existente!");
+                            return;
+                        }
+                        q = $"INSERT INTO persona_registrada VALUES (" +
+                                $"'{nombre}', " +
+                                $"'{apMaterno}', " +
+                                $"'{apPaterno}', " +
+                                $"'{fechaNac?.ToString("yyyy-MM-dd")}', " + // Format the date for SQL
+                                $"'{horaNac?.ToString()}', " + // Use the default TimeSpan.ToString() format
+                                $"'{crip}', " +
+                                $"'{nuCurp}', " +
+                                $"'{curpMadre}', " +
+                                $"'{curpPadre}', " +
+                                $"'{municipio}', " +
+                                $"'{genero}', " +
+                                $"'{presentado}', " +
+                                $"{oficialia}," +
+                                $"{numActa}, " +
+                                $"{numLibro}," +
+                                $"'{fechaReg?.ToString("yyyy-MM-dd")}')";
+                        if(ExecQuery(conn, q))
+                        {
+                            new ShoInfoMsg(ShoInfoMsg.SUCCESS, "Dato insertado con exito!");
+                        }
+                        break;
+                }
+            }
         }
+
+        private string GenerarCurp(string apPaterno, string apMaterno, string nombre, DateTime? fechaNac, string genero, string municipio)
+        {
+            Random rnd = new Random();
+            string iniciales = $"{apPaterno[0]}";
+
+            for (int i = 1; i < apPaterno.Length; i++)
+            {
+                if ("AEIOU".Contains(apPaterno[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    iniciales += char.ToUpper(apPaterno[i]);
+                    break;
+                }
+            }
+
+            string estado = municipios.FirstOrDefault(m => m._id == municipio)._entidad;
+
+            iniciales += $"{apMaterno[0]}{nombre[0]}".ToUpper();
+
+            // Obtener los dígitos del año de nacimiento
+            string anioNac = (fechaNac?.Year % 100)?.ToString("D2");
+
+            // Obtener el carácter del mes de nacimiento (en el caso de enero, se utiliza "0")
+            string mesNac = fechaNac?.Month == 1 ? "0" : fechaNac?.Month.ToString("D2");
+
+            // Obtener los dígitos del día de nacimiento
+            string diaNac = fechaNac?.Day.ToString("D2");
+            string consonantes = $"{ObtenerConsonante(apPaterno.Substring(1))}{ObtenerConsonante(apMaterno)}{ObtenerConsonante(nombre)}";
+
+            // Construir el string de la CURP
+            return $"{iniciales}{anioNac}{mesNac}{diaNac}{genero[0]}{estado.Substring(0,2)}{consonantes}{iniciales[0]}{rnd.NextInt64(10,99)}";
+        }
+
+        private char ObtenerConsonante(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!EsVocal(s[i]))
+                {
+                    return char.ToUpper(s[i]);
+                }
+            }
+            return '\0';
+        }
+
+        bool EsVocal(char letra)
+        {
+            return "AEIOUaeiou".Contains(letra);
+        }
+
 
         private bool ExecQuery(SqlConnection conn, string query)
         {
@@ -183,22 +334,5 @@ namespace ProyectoAdminBD.MVVM.View
             return false;
         }
 
-        private void HORANAC_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            string currentText = textBox.Text;
-
-            // Remove any existing colons from the text
-            currentText = currentText.Replace(":", "");
-
-            // Insert colons every 2 characters
-            if (currentText.Length > 1)
-            {
-                currentText = currentText.Insert(2, ":");
-            }
-
-            // Set the formatted text back to the TextBox
-            textBox.Text = currentText;      
-        } 
     }
 }
